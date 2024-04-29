3fd12360de87c2d8994fbafa089449c61a370541
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-04-29 09:23:11 -0400

    prompts and env

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.experimental.query_engine import PandasQueryEngine
+load_dotenv()
 
-print('you bet brother')
+popultion_path = os.path.join("data", "WorldPopulation2023.csv")
+population_df = pd.read_csv(popultion_path)
+
+#verbose show how conclusion was made
+population_query_engine = PandasQueryEngine(df= population_df, verbose=True )
+
+print('you bet brother:  ', population_df.head())
