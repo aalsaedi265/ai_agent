f8b962a13efa9625098ffb7dadb88d3dcefd0d31
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-04-30 11:05:36 -0400

    first test sucessful

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from llama_index.experimental.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str
 load_dotenv()
 
-popultion_path = os.path.join("data", "WorldPopulation2023.csv")
-population_df = pd.read_csv(popultion_path)
+# Load the data
+population_path = os.path.join("data", "WorldPopulation2023.csv")
+population_df = pd.read_csv(population_path)
 
-#verbose show how conclusion was made
-population_query_engine = PandasQueryEngine(df= population_df, verbose=True )
+# Initialize the query engine
+population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
 
+# Use the query engine to make a query
+result = population_query_engine.query('what is the population of usa')
+
+# Output the result and show the first few rows of the dataframe
 print('you bet brother:  ', population_df.head())
