c1c800e6c544ff5100c512e390003aa3abf19a88
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-05-02 12:18:17 -0400

    querying

---
+++ pdf.py
@@ -0,0 +1,8 @@
+
+import os
+from llama_index.core import StorageContext, vector_stores, load_index_from_storage
+#found on llama hub
+from llama_index.readers import PDFReader
+
+#this is jack of all trades, it it does not work for you go to llama hub and you will find what yo need 
+#from llama_index import SimpleDirectoryReader
